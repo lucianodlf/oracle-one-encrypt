fcea89e574a8b7c4bfc7a145324307702703485c
Luciano Delfino <luciano.dlf@gmail.com>
2023-04-27 09:31:55 -0300

    Add encriptado y desencriptado de texto
    * Add validacion de texto

--- js/main.js
+++ js/main.js
@@ -1,26 +1,69 @@
-const encrypText =  (text) => {
+// Characters to replace
+const character_to_replace = ["e", "i", "a", "o", "u"];
+
+//  Words to replace
+const words_replace = ["enter", "imes", "ai", "ober", "ufat"];
+
+const verifyCharacters = (text) => {
+    /* Regex que permite solo letras minusculas, sin acentos, sin caracteres especiales */
+    const regex = /^[a-z\s]*$/
+    return regex.test(text);
+}
+
+const encrypText = (text) => {
+    for (let i = 0; i < character_to_replace.length; i++) {
+        text = text.replaceAll(character_to_replace[i], words_replace[i]);
+    }
+    console.debug(`Encrypted text: ${text}`);
     return text;
 }
 
 const decryptText = (text) => {
+    for (let i = 0; i < words_replace.length; i++) {
+        text = text.replaceAll(words_replace[i], character_to_replace[i]);
+    }
+    console.debug(`Decrypted text: ${text}`);
+    
     return text;
 }
 
 const encrypt = () => {
-    const  text = document.getElementById("text-to-encrypt").value;
-    console.log(text);
+    const text = document.getElementById("text-to-encrypt").value.trim();
+    console.debug(`Text to encrypt: ${text}`);
+
+    if (!verifyCharacters(text)) {
+        alert("Texto invalido");
+        return;
+    }
+
+    const encryptedText = encrypText(text);
+    document.getElementById("encrypted-text").value = encryptedText;
+
     return;
 }
 
 const decrypt = () => {
+    const text = document.getElementById("text-to-encrypt").value.trim();
+    console.debug(`Text to decrypt: ${text}`);
+
+    if (!verifyCharacters(text)) {
+        alert("Texto invalido");
+        return;
+    }
+
+    const decryptedText = decryptText(text);
+    document.getElementById("encrypted-text").value = decryptedText;
+
     return;
 }
 
-// funcion habitual para ejecutar javascript despues de que se carga el documento
-window.onload = () => {
-  console.log('Documment cargado')
 
-  const encryptButton = document.getElementById("encrypt-button");
-  encryptButton.onclick = encrypt;
-  
+window.onload = () => {
+
+    const encryptButton = document.getElementById("encrypt-button");
+    encryptButton.onclick = encrypt;
+
+    const decryptButton = document.getElementById("decrypt-button");
+    decryptButton.onclick = decrypt;
+
 }  
